16d19d67cf7c4b9ea5d84d4ff3a3a8bd3ece8b2f
Viet Hoang <hoangviet18306@gmail.com>
2020-11-22 12:56:18 +0700

    update Ultilities based on before

--- Utility.pas
+++ Utility.pas
@@ -1,4 +1,6 @@
 // determind Prime
+// Input: n:integer(so nao cũng được)
+// Output: result:boolean(bool) => trả về kết quả rằng đó là số nguyên tố (true) và ngược lại (false)
 function prime (n:integer):boolean;
 var x:integer; result:boolean;
 begin
@@ -9,6 +11,8 @@ begin
 end;
 
 // determined Reverse
+// Input: n:integer(so nao cũng được)
+// Output: result:integer(so nghịch đảo của số nào cũng được)
 function reverse (n:integer):integer;
 var x:integer; result:integer;
 begin
@@ -24,6 +28,8 @@ begin
 end;
 
 // perfect numbers(function to check)
+// Input: number needs to check
+// Output: result value (true/false) determind that is n an perfect number (số hoàn hảo)
 function prefectnum (n:integer):boolean;
 var sum,x:integer; result:boolean;
 begin
@@ -37,6 +43,8 @@ end;
 
 // with 3 arrays and l1 l2 and l3 numbers of elements in each arrays 
 // give it into 1 array
+// Input: a,b,c:array[numbers of elements] of (type of data) and a,b,c numbers of elements in each array
+// Output: compound array(ghep) contist of a(am),b(ko),c(duong)
 procedure compoundArray;
 var l1,l2,l3:integer // must have it before !!!
 begin
@@ -49,6 +57,8 @@ begin
 end;
 
 // change the position of 2 elements of an array
+// Input: x,y(2 numbers or elements need to change)
+// Output: x(y),y(x) are numbers or element after switch
 procedure change(var x,y: string);//string are integer up to you!
 var t: string;// t is temporary value; x,y are values need to switch.
 begin
@@ -63,7 +73,89 @@ var i,j:integer;// position of array1 and array2
 begin
       for i:=1 to n-1 do
         for j:=i+1 to n do
-        if (a[i]+a[j] < a[j]+a[i]) then
-                hoanvi(a[i],a[j]);
+        if (a[i]+a[j] < a[j]+a[i]) then //also uses for compound string 
+                change(a[i],a[j]);
 
+end;
+
+// determind the number which is the number call Số chính phương
+// input: n:integer (number need to determind)
+// Output: is it a số chính phương (true/false)
+function squarenumber(n:integer):boolean;
+var x:integer; result:boolean;
+begin
+    result:=false;
+    x:=0;
+    while x*x <= n do
+    begin
+        if x*x = n then result:= true;
+        x:= x+1;
+    end;
+    squarenumber:= result;
+end;
+
+//fibonancci numbers
+procedure fibo(var k:longint);
+begin
+ f1:=1;
+ f2:=1;
+ for i:=1 to k do
+ begin
+  f3:=f1+f2;
+  write(f1:3,' ');
+  f1:=f2;
+  f2:=f3;
+ end;
+end;
+
+//send back the factorial of an arrange
+// Input: n:integer (arrange)
+// Output: the numbers after we factorial its(result:inter)
+function factorial(n:integer):integer;
+var x:integer; result:integer;
+begin
+    result:=1;
+    for x:=1 to n do
+    begin
+        result:= result*x;
+    end;
+    factorial:=result;
+end;
+
+//search how many time is an element repeat in an array
+// input: a:array (of element), x:integer (the head number)
+// output: how many times it repeated (x)
+function timtanxuat(a:mang; x:integer):integer;
+var i,tong:integer;
+begin
+    tong:=0;
+    for i:=1 to n do
+    if a[i]=x then tong:=tong+1;
+    timtanxuat:=tong;
+end;
+
+//find the distance between a and b
+// Input: number a,b :longint (number need to caculate)
+// Output: distance:longint (distance between 2 object (a,b))
+function khoangcach(a,b:longint):longint;
+var distance:longint;
+begin
+    if a>=b then distance:=a-b
+    else if a<b then distance:=b-a;
+    khoangcach:=distance;
+end;
+
+//function find how many numbers arrange increasingly
+function increaselength(n:integer):integer;
+var i,j,tong,max:integer;
+begin
+  tong:=1;
+  max:=0;
+  for i:=1 to n-1 do
+    if (a[i]<=a[i+1]) then
+      begin
+      tong:=tong+1;
+      if tong>max then max:=tong;
+      end;
+    increaselength:=max;
 end;
